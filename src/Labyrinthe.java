1cdef31c758521352b3811313c938f2e1a22ba20
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2025-10-07 11:47:43 +0200

    ajoute la base de la classe Labyrinthe

---
+++ src/Labyrinthe.java
@@ -0,0 +1,25 @@
+import Cellules.Cellule;
+
+public class Labyrinthe {
+
+    private final int largeur;
+    private final int hauteur;
+    private final double pourcentageMurs;
+    private final int distanceMin;
+    private Cellule[][] cellules;
+
+    public Labyrinthe(int largeur, int hauteur, double pourcentageMurs) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.pourcentageMurs = pourcentageMurs;
+    }
+
+    public void generer() {
+        //TODO
+    }
+
+    public void calculePlusCourtChemin() {
+        //TODO
+        //this.distanceMin = ;
+    }
+}
