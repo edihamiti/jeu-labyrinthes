783a6d3a8a47b4f760c7fe5348629bbc44427ef1
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2025-10-07 11:48:44 +0200

    ajoute la classe abstraite Cellule

---
+++ src/Cellules/Cellule.java
@@ -0,0 +1,39 @@
+package Cellules;
+
+public abstract class Cellule {
+
+    private int x;
+    private int y;
+
+    public boolean estChemin() {
+        return false;
+    }
+
+    public boolean estMur() {
+        return false;
+    }
+
+    public boolean estEntree() {
+        return false;
+    }
+
+    public boolean estSortie() {
+        return false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
