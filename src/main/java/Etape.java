9d897b3d3ec3adcf6ef15cd1ec8b4c2db76ec7a9
amaury vanhoutte <amaury.vanhoutte.etu@univ-lille.fr>
2025-10-10 15:34:53 +0200

    Ajout classe Défi

--- src/main/java/Etape.java
+++ src/main/java/Etape.java
@@ -1,2 +1,17 @@
 public class Etape {
+    private Defi defi;
+    private int numero;
+
+    public Etape(Defi defi, int numero) {
+        this.defi = defi;
+        this.numero = numero;
+    }
+
+    public Defi getDefi() {
+        return defi;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
 }
