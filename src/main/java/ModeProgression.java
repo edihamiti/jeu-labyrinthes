0ada8cf8ecdbeea21c6fbba4c5a411739d8d06a5
amaury vanhoutte <amaury.vanhoutte.etu@univ-lille.fr>
2025-10-10 15:35:34 +0200

    Ajout classe ModeProgression

--- src/main/java/ModeProgression.java
+++ src/main/java/ModeProgression.java
@@ -1,2 +1,31 @@
-public class ModeProgression {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModeProgression extends ModeJeu {
+    private List<Etape> etapes = new ArrayList<>();
+
+    public ModeProgression() {
+        etapes.add(new Etape(Defi.FACILE1, 1));
+        etapes.add(new Etape(Defi.FACILE2, 2));
+        etapes.add(new Etape(Defi.FACILE3, 3));
+        etapes.add(new Etape(Defi.MOYEN1, 4));
+        etapes.add(new Etape(Defi.MOYEN2, 5));
+        etapes.add(new Etape(Defi.MOYEN3, 6));
+        etapes.add(new Etape(Defi.DIFFICILE1, 7));
+        etapes.add(new Etape(Defi.DIFFICILE2, 8));
+        etapes.add(new Etape(Defi.DIFFICILE3, 9));
+    }
+
+    public List<Etape> getEtapes() {
+        return etapes;
+    }
+
+    public Etape getEtape(int numero) {
+        for (Etape etape : etapes) {
+            if (etape.getNumero() == numero) {
+                return etape;
+            }
+        }
+        return null;
+    }
 }
