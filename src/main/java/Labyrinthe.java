79bce7c5895729ca161a9a58e8d54cecb29e88b9
Bastien Fernandes <bastien.fernandes.etu>
2025-10-10 14:17:10 +0200

    modif methode generer (juste contour mur)

--- src/main/java/Labyrinthe.java
+++ src/main/java/Labyrinthe.java
@@ -1,4 +1,6 @@
 import Cellules.Cellule;
+import Cellules.Entree;
+import Cellules.Mur;
 
 public class Labyrinthe {
 
@@ -15,7 +17,25 @@ public class Labyrinthe {
         this.distanceMin = distanceMin;
     }
 
-    public void generer() {
+    public void generer(int largeur, int hauteur, double pourcentageMurs) {
+        int largeurMax = largeur+2;
+        int hauteurMax = hauteur+2;
+        Cellule[][] sol = new Cellule[largeurMax][hauteurMax];
+        int nombreMursTotal = (int)((largeur*hauteur)*pourcentageMurs);
+        int nombreMurs = nombreMursTotal;
+        for(int i = 0; i<=largeurMax; i++){
+            sol[i][0]= new Mur(i,0);
+        }
+        for(int i = 0; i<=largeurMax; i++){
+            sol[i][hauteurMax]= new Mur(i,0);
+        }
+        for(int i = largeur; i<=hauteurMax; i++){
+            sol[largeurMax][i]= new Mur(i,0);
+        }
+        for(int i = 0; i<=hauteurMax; i++){
+            sol[0][i]= new Mur(i,0);
+        }
+        sol[1][0]= new Entree(1,0);
         //TODO
     }
 
