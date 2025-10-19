bbebc81d94bc81f333a435daa1d12a139d953a71
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2025-10-19 17:14:20 +0200

    Ajout de la gestion de la progression pour la classe Joueur

--- src/main/java/modele/Joueur.java
+++ src/main/java/modele/Joueur.java
@@ -2,18 +2,28 @@ package modele;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class Joueur {
 
     private static int count = 1;
     private final int id;
     private String pseudo;
+    private HashMap<Defi, Boolean> progression;
 
     public Joueur(String pseudo) throws PseudoException {
         this.id = count++;
-        if (pseudo == null || pseudo.trim().isEmpty()) throw new PseudoException("Le pseudo ne peut pas être vide ou compose que d'espace");
+        if (pseudo == null || pseudo.trim().isEmpty())
+            throw new PseudoException("Le pseudo ne peut pas être vide ou compose que d'espace");
         else if (pseudo.length() > 15) throw new PseudoException("Le pseudo est trop long");
-        else if (!pseudo.matches("[a-zA-Z0-9]+")) throw new PseudoException("Le pseudo ne peut contenir que des lettres et des chiffres");
+        else if (!pseudo.matches("[a-zA-Z0-9]+"))
+            throw new PseudoException("Le pseudo ne peut contenir que des lettres et des chiffres");
         else this.pseudo = pseudo;
+
+        this.progression = new HashMap<>();
+        for (Defi defi : Defi.values()) {
+            this.progression.put(defi, false);
+        }
     }
 
     public Joueur(JSONObject joueur){
@@ -33,6 +43,14 @@ public class Joueur {
         this.pseudo = pseudo;
     }
 
+    public HashMap<Defi, Boolean> getProgression() {
+        return progression;
+    }
+
+    public void setProgression(HashMap<Defi, Boolean> progression) {
+        this.progression = progression;
+    }
+
     public JSONObject toJson(){
         JSONObject joueur = new JSONObject();
         joueur.put("id", this.id);
@@ -40,4 +58,12 @@ public class Joueur {
         return joueur;
     }
 
+    @Override
+    public String toString() {
+        return "Joueur{" +
+                "id=" + id +
+                ", pseudo='" + pseudo + '\'' +
+                ", progression=" + progression +
+                '}';
+    }
 }
