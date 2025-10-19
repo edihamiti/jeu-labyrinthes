349010e56eb3a1a049f6a11f6fca116199b541cb
Edi Hamiti <edihamiti@gmail.com>
2025-10-19 12:20:57 +0200

    Ajout de l'interface LabyrinthesObservateur

--- src/main/java/modele/Jeu.java
+++ src/main/java/modele/Jeu.java
@@ -8,6 +8,7 @@ public class Jeu {
     private ModeJeu modeJeu;
     private Joueur joueur;
     private Labyrinthe labyrinthe;
+    private ArrayList<LabyrinthesObservateur> observateur;
 
     public Jeu(ModeJeu modeJeu, Joueur joueur, Labyrinthe labyrinthe) {
         this.modeJeu = modeJeu;
