6a15eb60006cf9f167764d3924fee3dde5073de8
Edi Hamiti <edihamiti@gmail.com>
2025-10-19 12:21:11 +0200

    Update Main.java

--- src/main/java/modele/Main.java
+++ src/main/java/modele/Main.java
@@ -2,6 +2,7 @@ package modele;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue dans le jeu du modele.Labyrinthe");
+        System.out.println("Bienvenue dans le jeu du Labyrinthe");
     }
+    // Je sais pas à quoi sert cette classe ducoup...
 }
