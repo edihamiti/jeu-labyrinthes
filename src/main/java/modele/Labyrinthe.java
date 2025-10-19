f50ccd3a228c85026b47875540d0460e116b15b0
amaury vanhoutte <amaury.vanhoutte.etu@univ-lille.fr>
2025-10-19 20:00:04 +0200

    implementation calculCheminPlusCourt() w/ Dijkstra

--- src/main/java/modele/Labyrinthe.java
+++ src/main/java/modele/Labyrinthe.java
@@ -2,7 +2,10 @@ package modele;
 
 import modele.Cellules.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.LinkedList;
 
 public class Labyrinthe {
 
@@ -10,38 +13,37 @@ public class Labyrinthe {
     private final int hauteur;
     private final double pourcentageMurs;
     private final int distanceMin;
-    private Cellule[][] cellules;
-
     private final int largeurMax;
     private final int hauteurMax;
     private final double nbCaseChemin;
+    private Cellule[][] cellules;
 
     public Labyrinthe(int largeur, int hauteur, double pourcentageMurs, int distanceMin) {
         this.largeur = largeur;
         this.hauteur = hauteur;
         this.pourcentageMurs = pourcentageMurs;
         this.distanceMin = distanceMin;
-        this.largeurMax = largeur+2;
-        this.hauteurMax = hauteur+2;
-        this.nbCaseChemin =(int) ((largeur*hauteur)*((100-pourcentageMurs)/100));
+        this.largeurMax = largeur + 2;
+        this.hauteurMax = hauteur + 2;
+        this.nbCaseChemin = (int) ((largeur * hauteur) * ((100 - pourcentageMurs) / 100));
     }
 
     public void generer() {
         cellules = new Cellule[largeurMax][hauteurMax];
         for (int i = 0; i < largeurMax; i++) {
             for (int j = 0; j < hauteurMax; j++) {
-                cellules[i][j] = new Mur(i,j);
+                cellules[i][j] = new Mur(i, j);
             }
         }
         int x = 0;
         int y = 1;
-        cellules[0][1]= new Entree(x,y);
-        faireChemin(cellules,x,y);
+        cellules[0][1] = new Entree(x, y);
+        faireChemin(cellules, x, y);
 
         for (int i = 1; i < largeur; i++) {
             for (int j = 1; j < hauteur; j++) {
-                if ((!(cellules[i][j] instanceof Entree) && !(cellules[i][j] instanceof Sortie)) && (Math.random() < (1-(pourcentageMurs/100)))) {
-                    cellules[i][j] = new Chemin(x,y);
+                if ((!(cellules[i][j] instanceof Entree) && !(cellules[i][j] instanceof Sortie)) && (Math.random() < (1 - (pourcentageMurs / 100)))) {
+                    cellules[i][j] = new Chemin(x, y);
                 }
             }
         }
@@ -51,75 +53,146 @@ public class Labyrinthe {
     public void faireChemin(Cellule[][] cellules, int x, int y) {
         int cheminx = x;
         int cheminy = y;
-        if(x == 0){
+        if (x == 0) {
             cheminx += 1;
-        } else if (y == 0){
+        } else if (y == 0) {
             cheminy += 1;
         }
 
-        cellules[cheminx][cheminy] = new Chemin(cheminx,cheminy);
+        cellules[cheminx][cheminy] = new Chemin(cheminx, cheminy);
 
         boolean nouveauchemin;
         for (int i = 1; i <= nbCaseChemin; i++) {
-        nouveauchemin = false;
+            nouveauchemin = false;
             Random random = new Random();
             int sens = random.nextInt(4);
 
-            while(!nouveauchemin){
-                switch (sens){
+            while (!nouveauchemin) {
+                switch (sens) {
                     case 0: //Pour aller vers le haut
-                    if(cheminy-1 != 0){
-                        cheminy--;
-                        nouveauchemin = true;
-                    } else {
-                        nouveauchemin = false;
-                        sens += random.nextInt(3)+1;
-                    }
-                    break;
+                        if (cheminy - 1 != 0) {
+                            cheminy--;
+                            nouveauchemin = true;
+                        } else {
+                            nouveauchemin = false;
+                            sens += random.nextInt(3) + 1;
+                        }
+                        break;
                     case 1: //Pour aller vers la droite
-                    if(cheminx+1 != largeurMax){
-                        cheminx++;
-                        nouveauchemin = true;
-                    } else {
-                        nouveauchemin = false;
-                        int choix = random.nextInt(2);
-                        if(choix == 0)sens = 0;
-                        else sens += random.nextInt(2)+2;
-                    }
-                    break;
+                        if (cheminx + 1 != largeurMax) {
+                            cheminx++;
+                            nouveauchemin = true;
+                        } else {
+                            nouveauchemin = false;
+                            int choix = random.nextInt(2);
+                            if (choix == 0) sens = 0;
+                            else sens += random.nextInt(2) + 2;
+                        }
+                        break;
                     case 2: //Pour aller vers le bas
-                    if(cheminy+1 != hauteurMax){
-                        cheminy++;
-                        nouveauchemin = true;
-                    } else {
-                        nouveauchemin = false;
-                        int choix = random.nextInt(2);
-                        if(choix == 0)sens -=  random.nextInt(2)+1;
-                        else sens = 3;
-                    }
-                    break;
+                        if (cheminy + 1 != hauteurMax) {
+                            cheminy++;
+                            nouveauchemin = true;
+                        } else {
+                            nouveauchemin = false;
+                            int choix = random.nextInt(2);
+                            if (choix == 0) sens -= random.nextInt(2) + 1;
+                            else sens = 3;
+                        }
+                        break;
                     case 3: //Pour aller vers la gauche
-                    if(cheminx-1 != 0){
-                        cheminx--;
-                        nouveauchemin = true;
-                    } else {
-                        nouveauchemin = false;
-                        sens -= random.nextInt(3)+1;
-                    }
-                    break;
+                        if (cheminx - 1 != 0) {
+                            cheminx--;
+                            nouveauchemin = true;
+                        } else {
+                            nouveauchemin = false;
+                            sens -= random.nextInt(3) + 1;
+                        }
+                        break;
                 }
             }
 
-            if(i == nbCaseChemin){
-                cellules[cheminx][cheminy] = new Sortie(x,y);
+            if (i == nbCaseChemin) {
+                cellules[cheminx][cheminy] = new Sortie(x, y);
             } else {
                 cellules[cheminx][cheminy] = new Chemin(cheminx, cheminy);
             }
         }
     }
 
-    public void calculePlusCourtChemin() {
-        //TODO
-        //this.distanceMin = ;
+    public int calculePlusCourtChemin() {
+        int startX = -1;
+        int startY = -1;
+        int endX = -1;
+        int endY = -1;
+
+        for (int i = 0; i < largeurMax; i++) {
+            for (int j = 0; j < hauteurMax; j++) {
+                if (cellules[i][j] != null && cellules[i][j].estEntree()) {
+                    startX = i;
+                    startY = j;
+                }
+                if (cellules[i][j] != null && cellules[i][j].estSortie()) {
+                    endX = i;
+                    endY = j;
+                }
+            }
+        }
+
+        if (startX == -1 || endX == -1) {
+            // Pas d'entrée ou sortie
+            return 0;
+        }
+
+        boolean[][] visited = new boolean[largeurMax][hauteurMax];
+        int[][] prevX = new int[largeurMax][hauteurMax];
+        int[][] prevY = new int[largeurMax][hauteurMax];
+        int[][] dist = new int[largeurMax][hauteurMax];
+        for (int i = 0; i < largeurMax; i++) {
+            for (int j = 0; j < hauteurMax; j++) {
+                prevX[i][j] = -1;
+                prevY[i][j] = -1;
+                dist[i][j] = -1;
+            }
+        }
+
+        List<int[]> queue = new ArrayList<>();
+        queue.add(new int[]{startX, startY});
+        visited[startX][startY] = true;
+        dist[startX][startY] = 0;
+
+        int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] courant = queue.removeFirst();
+            int cx = courant[0];
+            int cy = courant[1];
+
+            if (cx == endX && cy == endY) {
+                break;
+            }
+
+            for (int[] d : direction) {
+                int nx = cx + d[0];
+                int ny = cy + d[1];
+
+                if (nx < 0 || nx >= largeurMax || ny < 0 || ny >= hauteurMax) continue;
+                if (visited[nx][ny]) continue;
+                if (cellules[nx][ny] == null) continue;
+                if (cellules[nx][ny].estMur()) continue;
+
+                visited[nx][ny] = true;
+                prevX[nx][ny] = cx;
+                prevY[nx][ny] = cy;
+                dist[nx][ny] = dist[cx][cy] + 1;
+                queue.addLast(new int[]{nx, ny});
+            }
+        }
+
+        if (dist[endX][endY] == -1) {
+            // Pas de chemin trouvé
+            return 0;
+        }
+        return dist[endX][endY];
     }
 }
