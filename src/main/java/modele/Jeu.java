7b0115983871c509c3d6a5d7b4bc6814ed1157bb
Edi Hamiti <edihamiti@gmail.com>
2025-10-19 13:06:00 +0200

    Ajout de getters et setters et adders pour les observers

--- src/main/java/modele/Jeu.java
+++ src/main/java/modele/Jeu.java
@@ -15,4 +15,14 @@ public class Jeu {
         this.joueur = joueur;
         this.labyrinthe = labyrinthe;
     }
+
+    public ModeJeu getModeJeu() {return modeJeu;}
+    public void setModeJeu(ModeJeu modeJeu) {this.modeJeu = modeJeu;}
+    public Joueur getJoueur() {return joueur;}
+    public void setJoueur(Joueur joueur) {this.joueur = joueur;}
+    public Labyrinthe getLabyrinthe() {return labyrinthe;}
+    public void setLabyrinthe(Labyrinthe labyrinthe) {this.labyrinthe = labyrinthe;}
+    public void addObservateur(LabyrinthesObservateur o){observateur.add(o);}
+    public void removeObservateur(LabyrinthesObservateur o){observateur.remove(o);}
+
 }
