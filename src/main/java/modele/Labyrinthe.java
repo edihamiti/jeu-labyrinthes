9edc28e59832c883249f6abdd75a973e2375e513
Bastien Fernandes <bastien.fernandes.etu@univ-lille.fr>
2025-10-19 16:02:27 +0000

    Debut methode générer labyrinthe et création methode faireChemin

--- src/main/java/modele/Labyrinthe.java
+++ src/main/java/modele/Labyrinthe.java
@@ -1,6 +1,8 @@
 package modele;
 
 import modele.Cellules.Cellule;
+import modele.Cellules.Entree;
+import modele.Cellules.Mur;
 
 public class Labyrinthe {
 
@@ -18,7 +20,19 @@ public class Labyrinthe {
     }
 
     public void generer() {
-        //TODO
+        cellules = new Cellule[largeur+2][hauteur+2];
+        for (int i = 0; i < largeur+2; i++) {
+            for (int j = 0; j < hauteur+2; j++) {
+                cellules[i][j] = new Mur(i,j);
+            }
+        }
+        cellules[0][1]= new Entree(0,1);
+        faireChemin(cellules);
+    }
+
+
+    public void faireChemin(Cellule[][] cellules) {
+
     }
 
     public void calculePlusCourtChemin() {
