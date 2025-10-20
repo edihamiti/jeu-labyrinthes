b68ded5cea6f79f6486bca69c32c669ce931093f
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2025-10-21 00:28:54 +0200

    Il est maintenant possible de jouer au labyrinthe dans javafx #6 #2

--- src/main/java/controleur/JeuControleur.java
+++ src/main/java/controleur/JeuControleur.java
@@ -3,6 +3,7 @@ package controleur;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import modele.Cellules.Cellule;
@@ -12,14 +13,13 @@ import java.io.IOException;
 
 public class JeuControleur {
 
-    @FXML
-    private VBox contienLabyrinthe;
-
-    private Labyrinthe labyrinthe;
-
     private final Image imgMur = new Image(getClass().getResourceAsStream("/img/mur.png"));
     private final Image imgChemin = new Image(getClass().getResourceAsStream("/img/chemin.png"));
     private final Image imgSortie = new Image(getClass().getResourceAsStream("/img/sortie.png"));
+    private final Image imgJoueur = new Image(getClass().getResourceAsStream("/img/joueur.png"));
+    @FXML
+    private VBox contienLabyrinthe;
+    private Labyrinthe labyrinthe;
 
     @FXML
     public void initialize() {
@@ -48,14 +48,62 @@ public class JeuControleur {
                 double x = j * tailleCellule;
                 double y = i * tailleCellule;
 
-                if (labyrinthe[i][j].estMur()) graphicsContext.drawImage(imgMur, x, y, tailleCellule, tailleCellule);
+                if (i == this.labyrinthe.getJoueurX() && j == this.labyrinthe.getJoueurY())
+                    graphicsContext.drawImage(imgJoueur, x, y, tailleCellule, tailleCellule);
+                else if (labyrinthe[i][j].estMur())
+                    graphicsContext.drawImage(imgMur, x, y, tailleCellule, tailleCellule);
                 else if (labyrinthe[i][j].estChemin())
                     graphicsContext.drawImage(imgChemin, x, y, tailleCellule, tailleCellule);
                 else if (labyrinthe[i][j].estSortie())
                     graphicsContext.drawImage(imgSortie, x, y, tailleCellule, tailleCellule);
+
             }
         }
+
         return canvas;
+    }
+
+    @FXML
+    public void deplacerHaut() throws IOException {
+        deplacer(labyrinthe.getJoueurX() - 1, labyrinthe.getJoueurY());
+    }
+
+    @FXML
+    public void deplacerBas() throws IOException {
+        deplacer(labyrinthe.getJoueurX() + 1, labyrinthe.getJoueurY());
+    }
+
+    @FXML
+    public void deplacerGauche() throws IOException {
+        deplacer(labyrinthe.getJoueurX(), labyrinthe.getJoueurY() - 1);
+    }
+
+    @FXML
+    public void deplacerDroite() throws IOException {
+        deplacer(labyrinthe.getJoueurX(), labyrinthe.getJoueurY() + 1);
+    }
+
+    private void deplacer(int nouveauX, int nouveauY) throws IOException {
+        if (labyrinthe.peutDeplacer(nouveauX, nouveauY)) {
+            labyrinthe.setJoueurX(nouveauX);
+            labyrinthe.setJoueurY(nouveauY);
+
+            afficherLabyrinthe();
+
+            if (labyrinthe.estSurSortie(nouveauX, nouveauY)) {
+                victoire();
+            }
+        }
+    }
+
+    private void victoire() throws IOException {
+        labyrinthe.setJeuEnCours(false);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Victoire");
+        alert.setHeaderText("Félicitations");
+        alert.setContentText("Vous avez trouvé la sortie");
+        alert.showAndWait();
+        AppControleur.getInstance().MenuPrincipal();
     }
 
 }
