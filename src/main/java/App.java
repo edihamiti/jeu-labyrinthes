e1891c16f70dd43f8bafd32a1258a59aa14f2571
Edi Hamiti <edihamiti@gmail.com>
2025-10-20 16:55:15 +0200

    Switch à un fxml main qui affichera les autres fxml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,9 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/HomePage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1400, 900);
-        stage.setTitle("HomePage");
+        stage.setTitle("Le Jeu des Labyrinthes");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
