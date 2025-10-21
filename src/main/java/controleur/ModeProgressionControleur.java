4780214145736e084083945a85015bafb86bdd6f
Bastien Fernandes <bastien.fernandes.etu@univ-lille.fr>
2025-10-21 08:48:50 +0000

    Ajout tableau choix defis

--- src/main/java/controleur/ModeProgressionControleur.java
+++ src/main/java/controleur/ModeProgressionControleur.java
@@ -6,5 +6,35 @@ import javafx.scene.layout.GridPane;
 
 public class ModeProgressionControleur {
 
+    @FXML
+    private GridPane grilleGrid;
 
+    @FXML
+    public void initialize() {
+        String[] colonnes = {"rien", "1", "2", "3"};
+        String[] lignes = {"facil", "moyen", "difficile"};
+
+        double prefWidth = 140;
+        double prefHeight = 100;
+
+        for (int col = 0; col < colonnes.length; col++) {
+            Button header = new Button(colonnes[col]);
+            header.setPrefSize(prefWidth, prefHeight);
+            grilleGrid.add(header, col, 0);
+        }
+
+        // Boutons OK
+        for (int row = 0; row < lignes.length; row++) {
+            Button rowHeader = new Button(lignes[row]);
+            rowHeader.setPrefSize(prefWidth, prefHeight);
+            grilleGrid.add(rowHeader, 0, row + 1);
+
+            for (int col = 1; col <= 3; col++) {
+                int niveau = col;
+                Button okButton = new Button("ok");
+                okButton.setPrefSize(prefWidth, prefHeight);
+                grilleGrid.add(okButton, col, row + 1);
+            }
+        }
+    }
 }
