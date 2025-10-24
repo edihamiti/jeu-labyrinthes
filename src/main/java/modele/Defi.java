f0d186bdf9ada962f7e02e77cd1ce0560e6fb65e
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2025-10-24 11:41:27 +0200

    ajout de la gestion de la vision dans l'énumération Defi

--- src/main/java/modele/Defi.java
+++ src/main/java/modele/Defi.java
@@ -5,22 +5,23 @@ package modele;
  * Chaque défi a une largeur, une hauteur, un pourcentage de murs et des points.
  */
 public enum Defi {
-    FACILE1(6, 6, 25.0, 5, 1),
-    MOYEN1(6, 6, 50.0, 15, 1),
-    DIFFICILE1(6, 6, 75.0, 45, 1),
+    FACILE1(6, 6, 25.0, 5, 1, Vision.VUE_LIBRE),
+    MOYEN1(6, 6, 50.0, 15, 1, Vision.VUE_LIBRE),
+    DIFFICILE1(6, 6, 75.0, 45, 1, Vision.VUE_LIBRE),
 
-    FACILE2(12, 12, 25.0, 10, 2),
-    MOYEN2(12, 12, 50.0, 20, 2),
-    DIFFICILE2(12, 12, 75.0, 50, 2),
+    FACILE2(12, 12, 25.0, 10, 2, Vision.VUE_LIBRE),
+    MOYEN2(12, 12, 50.0, 20, 2, Vision.VUE_LIBRE),
+    DIFFICILE2(12, 12, 75.0, 50, 2, Vision.VUE_LIBRE),
 
-    FACILE3(18, 18, 25.0, 15, 3),
-    MOYEN3(18, 18, 50.0, 25, 3),
-    DIFFICILE3(18, 18, 75.0, 55, 3);
+    FACILE3(18, 18, 25.0, 15, 3, Vision.VUE_LOCAL),
+    MOYEN3(18, 18, 50.0, 25, 3, Vision.VUE_LOCAL),
+    DIFFICILE3(18, 18, 75.0, 55, 3, Vision.VUE_LOCAL);
 
     private final int largeur;
     private final int hauteur;
     private final double pourcentageMurs;
     private final int points;
+    private final Vision vision;
     /*
     L'étape associée au défi (Pour rappel : chaque étape a 3 défis).
      */
@@ -33,12 +34,14 @@ public enum Defi {
      * @param hauteur         hauteur en cases
      * @param pourcentageMurs pourcentage de murs (0-100)
      * @param points          points attribués
+     * @param vision          mode de vision lors du jeu
      */
-    private Defi(int largeur, int hauteur, double pourcentageMurs, int points, int etape) {
+    private Defi(int largeur, int hauteur, double pourcentageMurs, int points, int etape, Vision vision) {
         this.largeur = largeur;
         this.hauteur = hauteur;
         this.pourcentageMurs = pourcentageMurs;
         this.points = points;
+        this.vision = vision;
         this.etape = etape;
     }
 
@@ -52,6 +55,10 @@ public enum Defi {
 
     public int getHauteur() {
         return hauteur;
+    }
+
+    public Vision getVision() {
+        return vision;
     }
 
     /**
