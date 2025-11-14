1e6c8b5e27518724aaa41ed36ae69de60bf8eb7d
Edi Hamiti <edi.hamiti.etu@univ-lille.fr>
2025-11-14 18:25:35 +0100

    Ajout de textures pour les cellules du labyrinthe

--- src/main/java/modele/Cellules/Piege.java
+++ src/main/java/modele/Cellules/Piege.java
@@ -1,13 +1,24 @@
 package modele.Cellules;
 
+import javafx.scene.image.Image;
+
 /**
  * Classe représentant une cellule de type piege dans un labyrinthe.
  */
 public class Piege extends Cellule {
+    static Image imageCache;
+    String imagePath = "/img/piege.png";
 
     public Piege(int x, int y) {
         this.setX(x);
         this.setY(y);
+    }
+
+    public Image getTexture() {
+        if (imageCache == null) {
+            imageCache = new Image(getClass().getResourceAsStream(imagePath));
+        }
+        return imageCache;
     }
 
     @Override
--- src/main/java/modele/Cellules/Sortie.java
+++ src/main/java/modele/Cellules/Sortie.java
@@ -1,13 +1,24 @@
 package modele.Cellules;
 
+import javafx.scene.image.Image;
+
 /**
  * Classe représentant une cellule de type sortie dans un labyrinthe.
  */
 public class Sortie extends Cellule {
+    static Image imageCache;
+    String imagePath = "/img/sortie.png";
 
     public Sortie(int x, int y) {
         this.setX(x);
         this.setY(y);
+    }
+
+    public Image getTexture() {
+        if (imageCache == null) {
+            imageCache = new Image(getClass().getResourceAsStream(imagePath));
+        }
+        return imageCache;
     }
 
     @Override
